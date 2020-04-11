1e05ccf5dc8fe946879068aa72c84b4e64a56edb
smc447 <61362038+smc447@users.noreply.github.com>
2020-04-11 12:23:35 -0400

    Add files via upload

---
+++ servoPractice.py
@@ -0,0 +1,24 @@
+print('started program')
+
+import RPi.GPIO as GPIO
+import time
+
+#setup
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(14, GPIO.OUT)
+p = GPIO.PWM(14,50)
+p.start(2.5)
+time.sleep(1)
+
+try:
+        while True:
+            #moves about 180 degrees back and forth
+            p.ChangeDutyCycle(4.5)
+            time.sleep(1)
+
+            p.ChangeDutyCycle(1.5)
+            time.sleep(1)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
