ffcec82d06a3e75b2aece5600e7aa86745b65509
smc447 <61362038+smc447@users.noreply.github.com>
2020-04-11 12:24:53 -0400

    Add files via upload
    This moves the servo about 60 degrees and is for the elbow joint of baby yoda.

---
+++ wavewithlib.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+void setup() {
+  // put your setup code here, to run once:
+ myservo.attach(9);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (pos = 120; pos <= 180; pos += 1) { // goes from 120 degrees to 180 degrees
+    //in steps of 1 degree
+    myservo.write(pos);              
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 120; pos -= 1) { // goes from 180 degrees to 120 degrees
+    myservo.write(pos);             
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
